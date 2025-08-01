1304eb8aea76ba83ec936bb7609272ecd5eec454
liqun1024 <liqun1024@foxmail.com>
2025-08-01 11:11:29 +0800

    taobao sim data update

--- my_exp/data_process/taobao_sim.py
+++ my_exp/data_process/taobao_sim.py
@@ -29,7 +29,7 @@ def find_top_k_similar_items(row, embeddings, k=20):
 
 if __name__ == "__main__":
     INPUT_DIR = "/home/liqun03/FuxiCTR/my_datasets/taobao"
-    OUTPUT_DIR = "/home/liqun03/FuxiCTR/my_datasets/taobao_sim"
+    OUTPUT_DIR = "/home/liqun03/FuxiCTR/my_datasets/taobao_sim_zero_based"
 
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
@@ -37,6 +37,9 @@ if __name__ == "__main__":
     item_embeddings = np.load("/home/liqun03/FuxiCTR/checkpoints/sasrec_item_embeddings.npy")
     for split in ['train', 'valid', 'test']:
         df = pd.read_parquet(os.path.join(INPUT_DIR, f'{split}.parquet'))
+
+        # delete 'item_hist' list if zero item
+        df['item_hist'] = df['item_hist'].apply(lambda x: [i for i in x if i > 0])
 
         results_df = df.apply(
             find_top_k_similar_items,
@@ -46,6 +49,14 @@ if __name__ == "__main__":
         )
 
         final_df = pd.concat([df, results_df], axis=1)
+        final_df = final_df[['user_id', 'target_item', 'item_hist', 'top_20_items', 'top_20_sims']]
+
+        # Adjusting indices to be zero-based
+        final_df['user_id'] = final_df['user_id'] - 1
+        final_df['target_item'] = final_df['target_item'] - 1
+        final_df['item_hist'] = final_df['item_hist'].apply(lambda x: [i - 1 for i in x])
+        final_df['top_20_items'] = final_df['top_20_items'].apply(lambda x: [i - 1 for i in x])
+
 
         output_filename = f'{split}_with_similarity.parquet'
         final_df.to_parquet(os.path.join(OUTPUT_DIR, output_filename), index=False)
